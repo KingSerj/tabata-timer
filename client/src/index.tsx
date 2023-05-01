a779c84a57ec1c86cc8c4cf7fbc43b7ea05001ba
Sergei <s.shologin@yandex.ru>
2023-05-01 19:00:18 +0600

    Add routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Timer} from "./pages/Timer";
+import {Settings} from "./pages/Settings";
+import {Programs} from "./pages/Programs";
+import {Root} from "./components/Root";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        children: [
+            // {
+            //   index: true,
+            //   element: <App/>
+            // },
+            {
+                path: "timer",
+                element: <Timer/>
+            },
+            {
+                path: "settings",
+                element: <Settings/>
+            },
+            {
+                path: "programs",
+                element: <Programs/>
+            }
+        ]
+    }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
