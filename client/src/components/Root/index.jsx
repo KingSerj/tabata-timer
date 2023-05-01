050a91e7b4ab33e461aae51eaccbf04dfad1b2f7
Sergei <s.shologin@yandex.ru>
2023-05-01 20:44:43 +0600

    Routing optimization

--- client/src/components/Root/index.jsx
+++ client/src/components/Root/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
+import * as SC from "./styles"
+import {Container} from "../Container";
 
 export const Root = () => <>
-    <div>Это будущее меню</div>
+    <Container>
+        <SC.Menu>
+            <SC.MenuItem to={"/timer"}>TIMER</SC.MenuItem>
+            <SC.MenuItem to={"/settings"}>SETTINGS</SC.MenuItem>
+            <SC.MenuItem to={"/programs"}>PROGRAMS</SC.MenuItem>
+        </SC.Menu>
+    </Container>
     <Outlet/>
 </>
